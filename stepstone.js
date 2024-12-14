548c43095aefc2a89637165d87225a887ec3c5c9
unknown <vishalvarwani8@gmail.com>
2024-12-14 14:19:56 +0100

    v

--- stepstone.js
+++ stepstone.js
@@ -11,7 +11,7 @@ const PORT = process.env.STEPSTONEPORT
 
 // Middleware
 const corsOptions = {
-    origin: ["http://localhost:3000"], // Replace with your frontend URL
+    origin: ["https://jobscanner-pb9s.onrender.com"], // Replace with your frontend URL
     methods: ["GET", "POST"],
     credentials: true
 };
